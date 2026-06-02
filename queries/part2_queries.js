f4cfbf5e5bf8221c37a33eebe9630e7b30ed26ea
Elizabeth2332 <20240492@dcfe.ie>
2026-06-02 16:16:17 +0100

    Update part2_queries.js
    added step 2 to part2_queries.js

--- queries/part2_queries.js
+++ queries/part2_queries.js
@@ -1,5 +1,6 @@
 use("spotify");
 
+// Завдання 1. Треки для вечірки
 print("\n=== Task 1: Party Tracks ===");
 
 db.tracks.find(
@@ -21,3 +22,35 @@ db.tracks.find(
     "audio_features.energy": 1
   }
 ).limit(20).forEach(printjson);
+
+// Завдання 2. Виконавці, у яких усі треки популярні
+print("\n=== Task 2: Artists with all popular tracks ===");
+
+db.tracks.aggregate([
+  { $unwind: "$artists" },
+  {
+    $group: {
+      _id: "$artists",
+      track_count: { $sum: 1 },
+      min_popularity: { $min: "$popularity" },
+      avg_popularity: { $avg: "$popularity" }
+    }
+  },
+  {
+    $match: {
+      track_count: { $gte: 3 },
+      min_popularity: { $gte: 60 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      artist: "$_id",
+      track_count: 1,
+      min_popularity: 1,
+      avg_popularity: { $round: ["$avg_popularity", 1] }
+    }
+  },
+  { $sort: { avg_popularity: -1 } },
+  { $limit: 20 }
+]).forEach(printjson);
