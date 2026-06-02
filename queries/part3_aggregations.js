981f4e034bd36409b2aa1483f504ea0097d4498f
Elizabeth2332 <20240492@dcfe.ie>
2026-06-02 16:34:24 +0100

    Update part3_aggregations.js
    task 2 queries/part3_aggregations.js

--- queries/part3_aggregations.js
+++ queries/part3_aggregations.js
@@ -1,5 +1,6 @@
 use("spotify");
 
+// Завдання 1. Топ-10 виконавців за середньою популярністю
 print("\n=== Part 3 Task 1: Top 10 Artists by Average Popularity ===");
 
 db.tracks.aggregate([
@@ -22,4 +23,62 @@ db.tracks.aggregate([
   },
   { $sort: { avg_popularity: -1 } },
   { $limit: 10 }
+]).forEach(printjson);
+
+// Завдання 2. Розподіл треків за настроєм
+print("\n=== Part 3 Task 2: Track Mood Distribution ===");
+
+db.tracks.aggregate([
+  {
+    $addFields: {
+      mood: {
+        $switch: {
+          branches: [
+            {
+              case: {
+                $and: [
+                  { $gte: ["$audio_features.valence", 0.5] },
+                  { $gte: ["$audio_features.energy", 0.5] }
+                ]
+              },
+              then: "happy"
+            },
+            {
+              case: {
+                $and: [
+                  { $lt: ["$audio_features.valence", 0.5] },
+                  { $gte: ["$audio_features.energy", 0.5] }
+                ]
+              },
+              then: "angry"
+            },
+            {
+              case: {
+                $and: [
+                  { $gte: ["$audio_features.valence", 0.5] },
+                  { $lt: ["$audio_features.energy", 0.5] }
+                ]
+              },
+              then: "calm"
+            }
+          ],
+          default: "sad"
+        }
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$mood",
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      mood: "$_id",
+      count: 1
+    }
+  },
+  { $sort: { count: -1 } }
 ]).forEach(printjson);
